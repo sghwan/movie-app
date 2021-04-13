9f38ec7c5f09458a6e2da0909709e5353c1c8ecf
sghwan <anan123456@naver.com>
2021-04-13 18:56:08 +0900

    initial commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,5 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'b784c934ddd779b7f2ece2928f8e579f'
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
